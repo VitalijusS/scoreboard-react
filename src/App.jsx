5b0cd74e4195f926788a09ea199d5f1bd320568d
VitalijusS <v.stifanovicius@gmail.com>
2024-08-08 12:07:55 +0300

    optimise

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,12 @@ function App() {
   const [count1, team1] = useState(0);
   const [count2, team2] = useState(0);
 
-  function team1Add1(){
-    team1(count1 + 1)
-  }
-  function team1Add2(){
-    team1(count1 + 2)
-  }
-  function team1Add3(){
-    team1(count1 + 3)
-  }
 
-  function team2Add1(){
-    team2(count2 + 1)
+  function addTeam2(num){
+    team2(count2 + num)
   }
-  function team2Add2(){
-    team2(count2 + 2)
-  }
-  function team2Add3(){
-    team2(count2 + 3)
+  function addTeam1(num){
+    team1(count1 + num)
   }
   return (
     <>
@@ -35,13 +23,13 @@ function App() {
             <p>Team two</p>
         </div>
         <div className="points">
-            <button onClick={team1Add3}>+3</button>
-            <button onClick={team1Add2}>+2</button>
-            <button onClick={team1Add1}>+1</button>
+            <button onClick={()=>addTeam1(3)}>+3</button>
+            <button onClick={()=>addTeam1(2)}>+2</button>
+            <button onClick={()=>addTeam1(1)}>+1</button>
             <p>|</p>
-            <button onClick={team2Add1}>+1</button>
-            <button onClick={team2Add2}>+2</button>
-            <button onClick={team2Add3}>+3</button>
+            <button onClick={()=>addTeam2(1)}>+1</button>
+            <button onClick={()=>addTeam2(2)}>+2</button>
+            <button onClick={()=>addTeam2(3)}>+3</button>
         </div>
     </div>
     </>
